954c0e74d900f6aa4eb2ba66fb5ca73996be7ffe
Fernando Ferrari <ferrari.cf91@outlook.com>
2022-07-27 14:49:45 -0300

    Criado Novo projeto para separaçõa de Classes, Models, Repository e Interface. Classes Cliente, Veiculos e Veiculo_Modelo Criados. Repositorio base criado para controle das funções. IRepositoryModel adicionando como Interface para uso das Funções.

---
+++ Localiza.Data/Interfaces/IRepositoryModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Localiza.Data.Interfaces
+{
+    public interface IRepositoryModel<T> where T : class
+    {
+        List<T> SelecionarTodos();
+
+        T SelecionarPrimaryKey(params object[] value);
+
+        T Incluir(T obj);
+
+        T Editar(T obj);
+
+        void Excluir(T obj);
+
+        void Excluir(params object[] value);
+
+        void Salvar();
+    }
+}
---
+++ Localiza.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Localiza.Data.Interfaces;
+
+namespace Localiza.Data.Repositories
+{
+    public class RepositoryBase<T> : IRepositoryModel<T>, IDisposable where T : class
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Editar(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(params object[] value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Incluir(T obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Salvar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T SelecionarPrimaryKey(params object[] value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> SelecionarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
