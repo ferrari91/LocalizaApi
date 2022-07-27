0b69a79f5cc5c1e09539b9d4f74970fbcb4dc608
Fernando Ferrari <ferrari.cf91@outlook.com>
2022-07-27 14:57:49 -0300

    Criado metodos no RepositoryBase

--- Localiza.Data/Repositories/RepositoryBase.cs
+++ Localiza.Data/Repositories/RepositoryBase.cs
@@ -4,49 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Localiza.Data.Interfaces;
+using Localiza.Data.Models;
 
 namespace Localiza.Data.Repositories
 {
     public class RepositoryBase<T> : IRepositoryModel<T>, IDisposable where T : class
     {
+        protected localizaDBContext _context;
+        public bool _Saves = true;
+
+        public RepositoryBase(bool saves = true)
+        {
+            _Saves = saves;
+            _context = new localizaDBContext();
+        }
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _context.Dispose();
         }
 
         public T Editar(T obj)
         {
-            throw new NotImplementedException();
+            _context.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+            if (_Saves)
+            {
+                _context.SaveChanges();
+            }
+
+            return obj;
         }
 
         public void Excluir(T obj)
         {
-            throw new NotImplementedException();
+           _context.Set<T>().Remove(obj);
+
+            if (_Saves)
+            {
+                _context.SaveChanges();
+            }
         }
 
         public void Excluir(params object[] value)
         {
-            throw new NotImplementedException();
+            var obj = SelecionarPrimaryKey(value);
+            Excluir(obj);
         }
 
         public T Incluir(T obj)
         {
-            throw new NotImplementedException();
+            _context.Set<T>().Add(obj);
+
+            if (_Saves)
+            {
+                _context.SaveChanges();
+            }
+            return obj;
         }
 
         public void Salvar()
         {
-            throw new NotImplementedException();
+            _context.SaveChanges();
         }
 
         public T SelecionarPrimaryKey(params object[] value)
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().Find(value);
         }
 
         public List<T> SelecionarTodos()
         {
-            throw new NotImplementedException();
+            return _context.Set<T>().ToList();
         }
     }
 }
