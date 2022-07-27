94d6a2eda5ee6b5c60405d742f265c750b4059eb
Fernando Ferrari <ferrari.cf91@outlook.com>
2022-07-27 11:30:22 -0300

    Adicionado EntityFrameWork Criado uma Classe de Context para Db.

--- LocalizaApi/Models/ClienteContext.cs
+++ LocalizaApi/Models/ClienteContext.cs
@@ -1,6 +1,18 @@
-﻿namespace LocalizaApi.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LocalizaApi.Models
 {
-    public class ClienteContext
+    public class BancoDadosContext:DbContext
     {
+        public BancoDadosContext(DbContextOptions<BancoDadosContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> tab_Cliente { get; set; }
+
+        public DbSet<Veiculos> tab_Veiculo { get; set; }
+
+        public DbSet<Veiculo_Modelo> tb_Veiculo_Modelo { get; set; }
     }
 }
