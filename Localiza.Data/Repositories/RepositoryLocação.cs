8aed8dca958107fae5f90212384289e89053a296
Fernando Ferrari <ferrari.cf91@outlook.com>
2022-07-27 23:29:07 -0300

    Adicionado uma rotina ao Editar para registrar o tempo e custo da locação dados as informações inseridas.

--- Localiza.Data/Repositories/RepositoryLocação.cs
+++ Localiza.Data/Repositories/RepositoryLocação.cs
@@ -1,5 +1,6 @@
 ﻿using Localiza.Data.Interfaces;
 using Localiza.Data.Models;
+using Localiza.Data.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,52 @@ namespace Localiza.Data.Repositories
         public RepositoryLocação(bool saves = true) : base(saves)
         {
 
+        }
+
+        public override List<TabLocacao> SelecionarTodos()
+        {
+            ServiceVeiculo service = new ServiceVeiculo();
+
+            var Listagem = base.SelecionarTodos();
+
+            foreach (var item in Listagem)
+            {
+                var veiculo = service._Repository.SelecionarPrimaryKey(item.CodigoVeiculo);
+                if (veiculo != null)
+                    item.CodigoVeiculoNavigation = veiculo;
+            }
+
+            return base.SelecionarTodos();
+        }
+
+        public override TabLocacao SelecionarPrimaryKey(params object[] value)
+        {
+            var locação = base.SelecionarPrimaryKey(value);
+            ServiceVeiculo service = new ServiceVeiculo();
+
+            var veiculo = service._Repository.SelecionarPrimaryKey(locação.CodigoVeiculo);
+            if (veiculo != null)
+                locação.CodigoVeiculoNavigation = veiculo;
+
+            return locação;
+        }
+
+        public override TabLocacao Editar(TabLocacao obj)
+        {
+            decimal horas = Convert.ToDecimal((obj.DataFinal - obj.DataInicial).Value.TotalHours);
+            var totalLiquido = obj.CodigoVeiculoNavigation.Valor * horas;
+            obj.Total = obj.CodigoVeiculoNavigation.Valor * horas;
+
+            if (obj.CarroLimpo == false)
+                obj.Total += (totalLiquido * 30) / 100;
+            if (obj.TanqueCheio == false)
+                obj.Total += (totalLiquido * 30) / 100;
+            if (obj.Amassados == true)
+                obj.Total += (totalLiquido * 30) / 100;
+            if (obj.Arranhoes == true)
+                obj.Total += (totalLiquido * 30) / 100;
+          
+            return base.Editar(obj);
         }
     }
 }
